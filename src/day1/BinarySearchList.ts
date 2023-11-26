dabd62c37aa4d2f11f0f3c94c922a44c88ed229a
Thiago Nebuloni <thiago.nebuloni@gmail.com>
2023-11-26 13:54:20 -0300

    Queue

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,19 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
+
+    do {
+        let m = Math.floor(lo + (hi - lo) / 2);
+        let v = haystack[m];
+
+        if (v === needle) {
+            return true
+        } else if (v > needle) {
+            hi = m
+        } else {
+            lo = m + 1
+        }
+    } while (lo < hi)
+    return false
+}
+
---
+++ src/day1/BubbleSort.ts
@@ -0,0 +1,15 @@
+export default function bubble_sort(arr: number[]): void {
+
+    const n = arr.length;
+
+    for (let i = 0; i < n; ++i) {
+        for (let j = 0; j < n + 1 + i; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                const tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+
+    }
+}
